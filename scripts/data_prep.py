6c1bde4e5cc77a645fc247ec4c8282372871e84e
benberinsky <tfu5hw@virginia.edu>
2026-04-17 14:38:44 -0400

    updating data prep script

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -43,4 +43,27 @@ def data_prep(data):
     # Drop duplicates
     data = data.drop_duplicates()
 
+    # Convert Attendance to numeric
+    data['attendance'] = pd.to_numeric(data['attendance'].str.replace('%', ''), errors='coerce')
+
+    # Define valid categories
+    valid_skills = ['Programming', 'Web development', 'Networking', 'Cyber security', 
+                    'Artificial Intelligence', 'Machine Learning', 'Software Development']
+
+    valid_interests = ['Software', 'Hardware', 'Data Science', 'UI/UX', 
+                    'NETWORKING', 'Machine Learning', 'Artificial Intelligence']
+
+    # Apply 'Other' conversion
+    data['skills'] = data['skills'].apply(lambda x: x if x in valid_skills else 'Other')
+    data['interest_area'] = data['interest_area'].apply(lambda x: x if x in valid_interests else 'Other')
+
+    # Encoding categorical variables
+    categorical_vars = ['english_proficiency', 'health_issues']
+    binary_vars = ['scholarship', 'gender', 'transportation', 'disabilities', ]
+    data = pd.get_dummies(data, columns=categorical_vars + binary_vars + ['skills', 'interest_area'], drop_first=True)
+
+
     return data
+
+prepared_data = data_prep(pd.read_csv("data/Students_Performance_dataset.csv"))
+prepared_data.to_csv("data/prepared_student_data.csv", index=False)
